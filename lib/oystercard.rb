d4ad7f65e4fd415c8acc3a0962e40fb60a7ac8b1
Joanne Chen <joanne_r_chen@yahoo.com>
2020-06-17 15:56:42 +0100

    Step 11 finished

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,14 +1,16 @@
 
 class Oystercard
 
-  attr_reader :balance
+  attr_reader :balance, :entry_station
   DEFAULT_LIMIT = 90
+  MINIMUM = 1
+  MIN_CHARGE = 1
 
-  def initialize(limit = DEFAULT_LIMIT)
+  def initialize(limit = DEFAULT_LIMIT, min = MINIMUM)
     @balance = 0
     @limit = limit
     @in_journey = false
-    # @touch_in = travel
+    @min = min  
   end
 
   def top_up(value)
@@ -21,15 +23,24 @@ class Oystercard
   end
  
   def in_journey?
-    @in_journey
+    @entry_station != nil
   end
 
-  def touch_in
+  def touch_in(station)
+    fail "Not enough funds available. Min fund is #{@min}." if @balance < MINIMUM
     @in_journey = true
+    @entry_station = station
   end
 
   def touch_out
+     deduct(MIN_CHARGE)
      @in_journey = false
+     @entry_station = nil
+  end
+
+  private
+  def deduct(amount)
+    @balance -= amount
   end
 
 end
--- spec/Oystercard_spec.rb
+++ spec/Oystercard_spec.rb
@@ -30,28 +30,73 @@ describe Oystercard do
   describe '#deduct' do
     it 'deducts money from the card' do
       oyster.top_up(20)
-      expect {oyster.deduct 1}.to change { oyster.balance }. by -1
+      expect {oyster.touch_out}.to change { oyster.balance }. by -1
     end 
   end
 
   describe '#in_journey' do
+    let(:station) {double "station"}
     it 'traveller begins with NOT in journey' do
       expect(oyster).not_to be_in_journey
     end
   
     it 'traveller touches in' do
-      oyster.touch_in
+      oyster.top_up(5)
+      oyster.touch_in(station)
       expect(oyster).to be_in_journey
     end
 
     it 'traveller touches out' do
-      oyster.touch_in
+      oyster.top_up(5)
+      oyster.touch_in(station)
       oyster.touch_out
       expect(oyster).not_to be_in_journey
     end
-  
   end
 
- 
+  describe '#touch in without enough funds' do 
+    let(:station) { double "station" }
+    it 'it raises an error if not enought money in oyster' do
+      min = Oystercard::MINIMUM
+      expect{ subject.touch_in(station) }.to raise_error "Not enough funds available. Min fund is #{min}."
+    end
+  end 
+
+  describe 'charge on touch out' do
+    let(:station) { double "station" }
+    it 'deduct min fare after touched out' do
+      oyster.top_up(5)
+      oyster.touch_in(station)
+      # oyster.touch_out
+      expect { oyster.touch_out }.to change { oyster.balance }.by (-Oystercard::MIN_CHARGE)
+    end
+  end
+
+  describe '#touch-in' do
+    it 'oyster.touch_in(station) to accept 1 arguement' do
+      expect(subject).to respond_to(:touch_in).with(1).argument
+    end
+  end
+
+  describe '#entry_station' do
+    let(:station) {double "station"}
+    it 'responds to entry station' do
+      expect(oyster).to respond_to(:entry_station) 
+    end
+
+    it 'stores the name store the name of station upon touch in' do
+      oyster.top_up(5)
+      oyster.touch_in(station)
+      expect(oyster.entry_station).to eq(station)
+    end
+   
+    it 'sets entry station to Nil upon touch out' do
+       oyster.top_up(5)
+       oyster.touch_in(station)
+       oyster.touch_out
+       expect(oyster.entry_station).to eq(nil)
+    end
+
+  end
 
 end
